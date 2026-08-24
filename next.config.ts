3fd316365e771472cb158b1a43570bd500f69f79
romeroalanpy-sketch <usuariotrabajoalan@gmail.com>
2026-08-24 18:11:15 -0300

    nibo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
   allowedDevOrigins: ['192.168.100.26', 'localhost:4000'],
 } as NextConfig;
 
